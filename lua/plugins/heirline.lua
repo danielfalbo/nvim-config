1832c64c30e7175a70c2ad6a5e2a606c5d71a646
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-08-26 22:47:31 +0200

    M lua/plugins/heirline.lua

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -1,26 +1,8 @@
--- The only diff from this and AstroNvim default is that this hides the close_button
--- TODO: this should just be an astroui option
-
+-- https://github.com/AstroNvim/AstroNvim/blob/2bb2fa9a01311ae7f9bfebf7b3ae996bcc4717be/lua/astronvim/plugins/heirline.lua#L131
 return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
     local status = require "astroui.status"
-    return require("astrocore").extend_tbl(opts, {
-      tabline = {
-        {  -- automatic sidebar padding
-          condition = function(self)
-            self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
-            self.winwidth = vim.api.nvim_win_get_width(self.winid)
-            return self.winwidth ~= vim.o.columns
-              and not require("astrocore.buffer").is_valid(vim.api.nvim_win_get_buf(self.winid))
-          end,
-          provider = function(self) return (" "):rep(self.winwidth + 1) end,
-          hl = { bg = "tabline_bg" },
-        },
-        status.heirline.make_buflist(status.component.tabline_file_info({
-          close_button = false,
-        })),
-      },
-    })
+    opts.tabline[2] = status.heirline.make_buflist(status.component.tabline_file_info({ close_button = false }))
   end,
 }
