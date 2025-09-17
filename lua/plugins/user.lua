c67df900eda165c954ca20397f9f4b5ea72f4ae2
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-09-17 07:17:06 +0200

    show hidden files in tree by default

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -4,6 +4,19 @@
 
 ---@type LazySpec
 return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
 
   -- -- == Examples of Adding Plugins ==
   --
